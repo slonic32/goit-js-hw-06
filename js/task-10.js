27b27d5c320e260384ebf3d41950fc140ab4ab43
Artem H <139376262+slonic32@users.noreply.github.com>
2023-12-01 23:44:21 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNumber = document.querySelector("#controls input");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const containerBoxes = document.querySelector("div#boxes");
+
+function createBoxes(amount) {
+  const baseSize = 30;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = baseSize + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxes.push(box);
+  }
+
+  containerBoxes.append(...boxes);
+}
+
+function destroyBoxes() {
+  containerBoxes.innerHTML = "";
+}
+
+buttonCreate.addEventListener("click", () => {
+  createBoxes(inputNumber.value);
+});
+
+buttonDestroy.addEventListener("click", destroyBoxes);
